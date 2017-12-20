b9db272621bc452b35a5391b9d7837c1b68c3833
张艳能 <ab052535zhangyanneng@ab-insurance.com>
2017-12-20 10:41:22 +0800

    收集工具类信息

---
+++ YNTools/TableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TableViewController.h
+//  YNTools
+//
+//  Created by 张艳能 on 2017/12/14.
+//  Copyright © 2017年 张艳能. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController
+
+@end
